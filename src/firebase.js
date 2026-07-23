f60bc029b818ca42a475d4ede3e780f0746cc302
Nathan Clairay <nathan.clairay@gmail.com>
2026-07-23 11:40:40 +0200

    Correction Firebase et ajout notifications

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,26 @@ import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
 
-  // colle ici les infos Firebase
+  apiKey: "AIzaSyBHVN5f_BWnXmtzkE0Ypf2WKniedjkS0lg",
+
+  authDomain: "pronos-notifs.firebaseapp.com",
+
+  projectId: "pronos-notifs",
+
+  storageBucket: "pronos-notifs.firebasestorage.app",
+
+  messagingSenderId: "104081341140",
+
+  appId: "1:104081341140:web:4c0b14e0d3ffcd7072d941",
+
+  measurementId: "G-QBW92H84KX"
 
 };
 
 
+
 const app = initializeApp(firebaseConfig);
+
 
 
 export const messaging = getMessaging(app);
